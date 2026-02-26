750777dfb85ba44264cdc7d6f4e831618b7c9a9f
matheusgonzales46 <matheusgaleite@gmail.com>
2026-02-26 17:42:05 -0300

    Update Dados primitivos.py

--- aula 2/Dados primitivos.py
+++ aula 2/Dados primitivos.py
@@ -1,27 +1,3 @@
-#dados primitivos em python
-
-#tipo string - texto
-
-'texto'
-"texto"
-
-#tipo intenger - inteiro
-
-print(1 + 2)
-2
-100
-
-#tipo float - real
-
-5.0
-2.10
-100.
-0.5
-
-#tipo bool - lógico
-True
-False
-
 
 print('olá mundo', 'nome')
 print('resultado da soma: ', 2+2)
@@ -33,3 +9,4 @@ print('R$', 500,)
 print('teste', 2, 5.2, False)
 
 print(1236, 'teste')
+
