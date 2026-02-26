bae57d7eb66f7f38dbf069b1874b869e78baf7ab
matheusgonzales46 <matheusgaleite@gmail.com>
2026-02-26 17:41:05 -0300

    Update Dados primitivos.py

--- aula 2/Dados primitivos.py
+++ aula 2/Dados primitivos.py
@@ -22,3 +22,14 @@ print(1 + 2)
 True
 False
 
+
+print('olá mundo', 'nome')
+print('resultado da soma: ', 2+2)
+print('resultado da divisão', 2/2)
+print('o aluno passou de ano?', True)
+print('altura: ', 1.72)
+print('R$', 500,)
+
+print('teste', 2, 5.2, False)
+
+print(1236, 'teste')
