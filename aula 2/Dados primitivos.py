8c714a65ac9ddb5764ba994929b1f134c90099e6
matheusgonzales46 <matheusgaleite@gmail.com>
2026-02-26 17:26:38 -0300

    Add files via upload

---
+++ aula 2/Dados primitivos.py
@@ -0,0 +1,24 @@
+#dados primitivos em python
+
+#tipo string - texto
+
+'texto'
+"texto"
+
+#tipo intenger - inteiro
+
+print(1 + 2)
+2
+100
+
+#tipo float - real
+
+5.0
+2.10
+100.
+0.5
+
+#tipo bool - lógico
+True
+False
+
